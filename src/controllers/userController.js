62d68eed8ed0e3c183e1f89449f040103ac90824
olioli13390 <olivier001@hotmail.fr>
2025-07-04 16:37:12 +0200

    create login.twig

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -4,5 +4,9 @@ const prisma = new PrismaClient({})
 exports.getRegister = async (req, res) => {
     const users = await prisma.user.findMany()
     res.render('pages/register.twig')
+}
 
+exports.getLogin = async (req, res) => {
+    const users = await prisma.user.findMany()
+    res.render('pages/login.twig')
 }
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -3,5 +3,6 @@ const router = express.Router()
 const userController = require("../controllers/userController")
 
 router.get('/register', userController.getRegister)
+router.get('/login', userController.getLogin)
 
 module.exports = router
